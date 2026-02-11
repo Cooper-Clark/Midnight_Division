585a32fa7d488133be432e59aedba5e0cfa96a00
dolloreanadog <dolloreanaseabass@gmail.com>
2026-02-11 16:22:46 -0700

    Testing if setup is good

--- Hello.cs
+++ Hello.cs
@@ -6,5 +6,7 @@ public partial class Hello : Node
     public override void _Ready()
     {
         GD.Print("Hello, World!"); 
+        GD.Print("Welcome to Godot with C#!");
+        GD.Print("This is a simple script to demonstrate C# scripting in Godot.");
     }
 }
