5f6e3691f1ec3727a03fe6991341fb79baa4043c
dolloreanadog <dolloreanaseabass@gmail.com>
2026-02-22 18:19:22 -0700

    Start on the Save System

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,11 +1,16 @@
 extends Node
 
-## This is a temporary holder information for when we eventually save or we load into with
+## This is a temporary holder information for when we eventually save or we load into with overall campaign
 var tempGameInfoDict: Dictionary[String, Variant]
+## This is a temporary holder information for ingame level, should be later stored within tempGameInfoDict
+var tempLevelInfoDict: Dictionary[String, Variant]
+
+## Filepath
+const savesFolderFilePath: String = "user://saves"
 
 func _ready() -> void:
 	# This will create a save folder if it doesn't exist
-	if (DirAccess.dir_exists_absolute("user://saves")):
+	if (DirAccess.dir_exists_absolute(savesFolderFilePath)):
 		print("A saves folder already exists")
 	else:
-		DirAccess.make_dir_absolute("user://saves")
+		DirAccess.make_dir_absolute(savesFolderFilePath)
