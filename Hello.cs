c652165eda6ad9da18b04da5683b31f5e5fe2f22
dolloreanadog <dolloreanaseabass@gmail.com>
2026-02-22 18:20:41 -0700

    Misc stuff
    CS file & window sizing

--- Hello.cs
+++ Hello.cs
@@ -3,10 +3,10 @@ using System;
 
 public partial class Hello : Node
 {
-    public override void _Ready()
-    {
-        GD.Print("Hello, World!"); 
-        GD.Print("Welcome to Godot with C#!");
-        GD.Print("This is a simple script to demonstrate C# scripting in Godot.");
-    }
+	public override void _Ready()
+	{
+		GD.Print("Hello, World!"); 
+		GD.Print("Welcome to Godot with C#!");
+		GD.Print("This is a simple script to demonstrate C# scripting in Godot.");
+	}
 }
